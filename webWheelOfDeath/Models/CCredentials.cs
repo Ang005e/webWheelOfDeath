b15318a6b9ce221304764739dab51ea786574ad2
Angus Blake <60034674@tafe.wa.edu.au>
2025-04-28 14:00:51 +0800

    Added CPlayer

--- webWheelOfDeath/Models/CCredentials.cs
+++ webWheelOfDeath/Models/CCredentials.cs
@@ -1,8 +1,27 @@
-﻿namespace webWheelOfDeath.Models
+﻿using Azure.Identity;
+using LibWheelOfDeath;
+
+namespace webWheelOfDeath.Models
 {
+    /// <summary>
+    /// Exposes appropriate API functions required by the web controller
+    /// 
+    /// </summary>
     public class CCredentials
     {
+        // ToDo: On login, initialise, and do Authenticate automatically -- set txtLoginSuccess property.
         public string txtPlayerUsername { get; set; } = string.Empty;
         public string txtPlayerPassword { get; set; } = string.Empty;
+        public bool loginAttemptFailed { get; set; } = false;
+
+        public void Authenticate(string username, string password)
+        {
+            CPlayer player = new CPlayer()
+            {
+                Username = username,
+                Password = password,
+            };
+            loginAttemptFailed = !player.Authenticate();
+        }
     }
 }
